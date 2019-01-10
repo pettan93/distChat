e4b6ab33f12cd37804dcf3a0713f4f9921724736
panhammer@seznam.cz <panhammer@seznam.cz>
2019-01-10 21:38:37 +0200

    test23

--- src/main/java/distChat/mySimuluations/SimulationConnect.java
+++ src/main/java/distChat/mySimuluations/SimulationConnect.java
@@ -6,8 +6,12 @@ import distChat.model.ChatRoom;
 import distChat.model.ChatRoomMessage;
 import distChat.model.ChatRoomParticipant;
 import distChat.model.ChatUser;
+import kademlia.node.KademliaId;
+import kademlia.node.Node;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Arrays;
 
 public class SimulationConnect {
@@ -21,8 +25,21 @@ public class SimulationConnect {
 
         ChatUser user1 = chatNodeBuilder.setNickName("Petr").setPort(7001).build();
 
-        UIController.buildUserController(Arrays.asList(user1));
-        UIController.initManager();
+//        UIController.buildUserController(Arrays.asList(user1));
+//        UIController.initManager();
+
+
+        System.out.println(user1.getKadNode().getNode().getSocketAddress());
+        System.out.println(user1.getKadNode().getOwnerId());
+        System.out.println(user1.getKadNode().getNode().getNodeId());
+
+
+        var id = new KademliaId(DigestUtils.sha1("Anna"));
+        String remoteIp="192.168.137.1";
+        InetAddress ip = InetAddress.getByName(remoteIp);
+        Node target = new Node(id, ip, Integer.parseInt("7001"));
+
+        user1.getKadNode().bootstrap(target);
 
     }
 
@@ -37,6 +54,10 @@ public class SimulationConnect {
     public static void main(String[] args) throws IOException {
 
         ChatUser user1 = chatNodeBuilder.setNickName("Anna").setPort(7001).build();
+
+        System.out.println(user1.getKadNode().getNode().getSocketAddress());
+        System.out.println(user1.getKadNode().getOwnerId());
+        System.out.println(user1.getKadNode().getNode().getNodeId());
 
         UIController.buildUserController(Arrays.asList(user1));
         UIController.initManager();
